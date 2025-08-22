7cc6f32beef0e25b030b047ea4e86ffc0ebbb840
Nicolas <alavreznicolas69@gmail.com>
2025-08-21 19:33:16 -0500

    prueba

--- App.js
+++ App.js
@@ -67,7 +67,80 @@ limpiarFormulario=()=>{
     [{texto:'OK'}]
    );
 
-}
+};
+render(){
+  const{nombre,
+    apellido,
+    email,
+    telefono,
+    descripcion,
+    mostrarPerfil,
+    mensajeAprobacion}=this.state;
+
+return(
+  <>
+    <ScrollView>
+      <View>
+        <Image
+          source={{uri:'ruta de imagen'}}
+          //style='se crea el estilo'
+          resizeMode='cover'
+        />
+        <Text>Mi perfil de usuario</Text>
+        <Text>App demo</Text>
+      </View>
+      <View>
+        <Text>Nombre</Text>
+        <TextInput
+          value={nombre}
+          onChangeText={manejarCambioNombre}
+          placeholder='Ingresa tu Nombre'
+          placeholderTextColor="#999"
+        />
+        <Text>Apellido</Text>
+        <TextInput
+          value={apellido}
+          onChangeText={manejarCambioApellido}
+          placeholder='Ingresa tu apellido'
+          placeholderTextColor="#999"
+        />
+        <Text>email</Text>
+        <TextInput
+          value={email}
+          onChangeText={manejarCambioEmail}
+          placeholder='Ingresa tu email'
+          placeholderTextColor="#999"
+        />
+        <Text>Telefono</Text>
+        <TextInput
+          value={telefono}
+          onChangeText={this.manejarCambioTelefono}
+          placeholder='Ingresa tu telefono'
+          placeholderTextColor="#999"
+        />
+        <Text>Descripcion</Text>
+        <TextInput
+          value={descripcion}
+          onChangeText={this.manejarCambioDescripcion}
+          placeholder='Ingresa tu descripcion'
+          placeholderTextColor="#999"
+        />
+      </View>
+      <View>
+        <Button
+          title='guardar perfil'
+          onPress={}// hace falta crear el metodo de guardar
+        />
+        <Button
+          title='Limpiar campos'
+          onPress={this.limpiarFormulario}  
+        />
+      </View>
+
+    </ScrollView>
+  </>
+)
+};
 
 
 };
